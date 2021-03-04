e32fe6ca805619ca4e774ad1776822076149a96f
waki akina <qbmht367598@gmail.com>
2021-03-04 22:21:18 +0900

    css切り替え

---
+++ js/jquery.js
@@ -0,0 +1,14 @@
+$(function(){
+ $('#red').click(function(){
+     $('.box').css('background-color','#FF0000');
+ });
+ $('#green').click(function(){
+     $('.box').css('background-color','#008000');
+ });
+ $('#blue').click(function(){
+     $('.box').css('background-color','#0000FF');
+ });
+ $('#reset').click(function(){
+     $('.box').css('background-color','#000');
+ });
+});
